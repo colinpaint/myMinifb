530ea26634171311ed1b92ab84707ca583039f46
colinpaint <88828347+colinpaint@users.noreply.github.com>
2022-12-11 14:56:14 +0000

    simpler

--- src/gl/MiniFB_GL.h
+++ src/gl/MiniFB_GL.h
@@ -1,13 +1,9 @@
 #pragma once
-#if defined(USE_OPENGL_API)
+#include <WindowData.h>
 
-  #include <WindowData.h>
+bool create_GL_context (SWindowData *window_data);
+void destroy_GL_context (SWindowData *window_data);
 
-  bool create_GL_context (SWindowData *window_data);
-  void destroy_GL_context (SWindowData *window_data);
-
-  void init_GL (SWindowData *window_data);
-  void redraw_GL (SWindowData *window_data, const void *pixels);
-  void resize_GL (SWindowData *window_data);
-
-#endif
+void init_GL (SWindowData *window_data);
+void redraw_GL (SWindowData *window_data, const void *pixels);
+void resize_GL (SWindowData *window_data);
